a39bc032a14c1f01ac8eacba375c5c917619522a
Mahendar Sura <mahendar.sura8@gmail.com>
2026-08-07 11:54:54 +0000

    Update terraform VPC and IAM modules

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -3,6 +3,88 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   enable_dns_support = true
+
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "three-tier-vpc"
+  }
+}
+
+
+resource "aws_internet_gateway" "main" {
+
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "three-tier-igw"
+  }
+}
+
+
+resource "aws_subnet" "public" {
+
+  count = 2
+
+  vpc_id = aws_vpc.main.id
+
+  cidr_block = [
+    "10.0.1.0/24",
+    "10.0.2.0/24"
+  ][count.index]
+
+  availability_zone = [
+    "us-east-1a",
+    "us-east-1b"
+  ][count.index]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "three-tier-public-${count.index + 1}"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+}
+
+
+resource "aws_route_table_association" "public" {
+
+  count = 2
+
+  subnet_id = aws_subnet.public[count.index].id
+
+  route_table_id = aws_route_table.public.id
+
+}
+
+
+output "vpc_id" {
+
+  value = aws_vpc.main.id
+
+}
+
+
+output "subnet_ids" {
+
+  value = aws_subnet.public[*].id
+
+}resource "aws_vpc" "main" {
+
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support = true
   enable_dns_hostnames = true
 
   tags = {
